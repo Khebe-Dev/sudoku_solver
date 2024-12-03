2b56ac9b5446b882ad8d4021dc0f6d584bef22a7
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2024-12-03 11:43:35 +0200

    files added

---
+++ solve.py
@@ -0,0 +1,5 @@
+# Tell users how to solve
+# Solving puzzle from path/to/puzzle.txt
+# Naked Single: Top right cell can only be a 4
+# Disambiguated Pair: The bottom row needs a 2 and a 6, but only one cell can be a 2. We fill in 6 and 2.
+# (etc, etc)
