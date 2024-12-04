2e290ad9f4462f0406fadf629bdfbd4bfb991696
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2024-12-04 23:12:15 +0200

    for loop to open input file

--- solve.py
+++ solve.py
@@ -2,21 +2,25 @@ import sys
 
 # sys.argv = ["solve.py", "path/to/puzzle.txt", "-o", "path/to/solved_puzzle.txt"]
 
-def main(filename):
+def main():
     try:
         filename = sys.argv[1]
-        with open(filename, "r") as input:
-            for lines in input:
+        with open(filename, "r") as input_file:
+            for lines in input_file:
+                lines = lines.strip()
                 print(lines)
 
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found")
+    except IndexError:
+        print("Error: Missing input file argument.")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Unexpected error: {e}")
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 solve.py path/to/puzzle.txt") #-o path/to/solved_puzzle.txt
     else:
-        main(sys.argv[1])
+        main()
