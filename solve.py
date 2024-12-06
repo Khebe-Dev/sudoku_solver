4bf37e7117c6b2a8c4a2dcd67f15854ef0d91998
jamolotcpt024 <jamolotcpt024@student.wethinkcode.co.za>
2024-12-06 13:52:43 +0200

    print grid

--- solve.py
+++ solve.py
@@ -38,6 +38,19 @@ def validity(grid,num,pos):
             if grid[raw][col] == num and (raw,col) != pos:
                 return False
     return True
+def print_grid(grid):
+
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("--------------------")
+            
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print("|", end="")
+            if j == 8:
+                print(grid[i][j])
+            else:
+                print(str(grid[i][j]) + " ", end="")
 
 def empty_spaces(grid):
     """ 
