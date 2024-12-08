39356f85fe467a0471b222f7c4c412c8a869d90e
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2024-12-08 14:52:58 +0200

    display grid

--- solve.py
+++ solve.py
@@ -1,43 +1,45 @@
 import sys
 
-# sys.argv = ["solve.py", "path/to/puzzle.txt", "-o", "path/to/solved_puzzle.txt"]
-
-def main():
-    # call open input file 
-    grid = open_input_file()
-    print(f"Solving puzzle from path/to/puzzle.txt: \n{grid}\n")
-
-    # call format grid 
-    print(format_grid(grid))
-
-
-
-def open_input_file():
+def read_input():
     try:
         input_filepath = sys.argv[1]
         with open(input_filepath, "r") as input_file:
-            grid = input_file.read().strip()
+            grid = input_file.read()
             return grid
 
-    except FileNotFoundError:
-        print(f"Error: File '{input_filepath}' not found")
-    except IndexError:
-        print("Error: Missing input file argument.")
     except Exception as e:
         print(f"Unexpected error: {e}") 
+        sys.exit(1)
 
 def format_grid(grid):
     sudoku_grid = []
     for lines in grid.splitlines():
-        # remove empty space, convert to integer of list
-        line_parts = list(map(int, lines.split()))
-        sudoku_grid.append(line_parts)
-
+        sudoku_grid.append([int(num) for num in lines.split()])
     return sudoku_grid
 
+def display_grid(grid):
+    for row in grid:
+        print(" ".join(str(num) for num in row))
+
+# def is_valid(grid, row, col, num):
+#     for i in range(9):
+
+# def row_check(sudoku_grid):
+#     for sudoku_grid in range(10):
+#         if set(sudoku_grid)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 solve.py path/to/puzzle.txt -o path/to/solved_puzzle.txt")
+        sys.exit(1)
+   
+    # read input file, format function for code use, display function for user understanding
+    grid = read_input()
+    sudoku_grid = format_grid(grid)
+    print(f"Solving puzzle from path/to/puzzle.txt: \n")
+    display_grid(sudoku_grid)
+
+ 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 solve.py path/to/puzzle.txt") #-o path/to/solved_puzzle.txt
-    else:
         main()
