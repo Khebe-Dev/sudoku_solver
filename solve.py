f1e68708050bb8051a6f184ecb9e3d8270ec634b
khhebecpt024 <khhebecpt024@student.wethinkcode.co.za>
2024-12-08 13:19:17 +0200

    continue with logic

--- solve.py
+++ solve.py
@@ -5,32 +5,35 @@ import sys
 def main():
     # call open input file 
     grid = open_input_file()
-    print(f"Unsolved sudoko puzzle: \n{grid}\n")
+    print(f"Solving puzzle from path/to/puzzle.txt: \n{grid}\n")
 
     # call format grid 
-    format_grid(grid)
+    print(format_grid(grid))
+
+
 
 def open_input_file():
     try:
-        filename = sys.argv[1]
-        with open(filename, "r") as input_file:
-            grid = input_file.read()
+        input_filepath = sys.argv[1]
+        with open(input_filepath, "r") as input_file:
+            grid = input_file.read().strip()
             return grid
 
     except FileNotFoundError:
-        print(f"Error: File '{filename}' not found")
+        print(f"Error: File '{input_filepath}' not found")
     except IndexError:
         print("Error: Missing input file argument.")
     except Exception as e:
         print(f"Unexpected error: {e}") 
 
 def format_grid(grid):
+    sudoku_grid = []
     for lines in grid.splitlines():
         # remove empty space, convert to integer of list
-        lines = list(map(int, lines.replace(" ", "")))
-        print(lines)
-        
+        line_parts = list(map(int, lines.split()))
+        sudoku_grid.append(line_parts)
 
+    return sudoku_grid
 
 
 if __name__ == "__main__":
